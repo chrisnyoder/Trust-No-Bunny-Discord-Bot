2c36c3ae8abb87a5e199aa4e02f38c8b0e6d845f
chrisnyoder <chrisnyoder@gmail.com>
2023-08-13 16:39:33 -0700

    discord bot now writes to a database

--- commands/claim.ts
+++ commands/claim.ts
@@ -9,7 +9,7 @@ const command = {
     async execute(interaction: CommandInteraction) {
         const items = ['item1', 'item2', 'item3', 'item4'];
         const randomItem = items[Math.floor(Math.random() * items.length)];
-        // await addNewClaim(interaction.user.id, randomItem);
+        await addNewClaim(interaction.user.id, randomItem);
         console.log('Claim successful for ' + interaction.user.id + ' with item ' + randomItem + '.');
         await interaction.reply('Claim successful! You earned ' + randomItem);
     }
