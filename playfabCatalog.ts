c273df782d9416a048bebae4a4c64fdbecd11101
chrisnyoder <chrisnyoder@gmail.com>
2023-08-14 22:36:22 -0700

    added retrieving items from Playfab

---
+++ playfabCatalog.ts
@@ -0,0 +1,46 @@
+import axios from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const PLAYFAB_URL_GET_TOKEN = 'https://DDD75.playfabapi.com/Authentication/GetEntityToken';
+const PLAYFAB_URL_SEARCH_ITEMS = 'https://DDD75.playfabapi.com/Catalog/SearchItems';
+const SECRET_KEY = process.env.PLAYFAB_SECRET_KEY;
+
+async function getEntityToken() {
+    try {
+        const response = await axios.post(PLAYFAB_URL_GET_TOKEN, {}, {
+            headers: {
+                'X-SecretKey': SECRET_KEY
+            }
+        });
+
+        return response.data.EntityToken;
+    } catch (error) {
+        console.error(`Error retrieving PlayFab entity token: ${error}`);
+        throw error;
+    }
+}
+
+export async function searchCatalogItems(): Promise<any> {
+    const entityToken = await getEntityToken();
+
+    try {
+        const response = await axios.post(PLAYFAB_URL_SEARCH_ITEMS, 
+            {
+                "Select": "images"
+            },
+            {
+                headers: {
+                    'X-EntityToken': entityToken,
+                    'Content-Type': 'application/json'
+                }
+            }
+        );
+
+        return response.data; // Modify as per the actual structure of the returned data
+    } catch (error) {
+        console.error(`Error retrieving PlayFab catalog items: ${error}`);
+        throw error;
+    }
+}
