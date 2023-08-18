0f45544bf7ba81c2aa913b17fbb226c9afa1ce73
chrisnyoder <chrisnyoder@gmail.com>
2023-08-18 14:18:51 -0700

    bugfixes

--- commands/claim.ts
+++ commands/claim.ts
@@ -40,7 +40,7 @@ const command = {
             return;
         }
 
-        await addNewClaim(interaction.user.id, item.reward_id, item.drop_id, item.reward_type);
+        await addNewClaim(item.drop_id, interaction.user.id, item.reward_id, item.reward_type);
         await setDropAsClaimed(item.drop_id);
         console.log('Claim successful for ' + interaction.user.id + ' with item ' + item.reward_id + '.');
     
