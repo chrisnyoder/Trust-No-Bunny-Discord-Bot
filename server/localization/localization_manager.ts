49d0634d533a1e93a3531b484358a3ca76e801b4
chrisnyoder <chrisnyoder@gmail.com>
2023-09-12 11:49:50 -0700

    fixing the path issues with languages again

--- server/localization/localization_manager.ts
+++ server/localization/localization_manager.ts
@@ -8,7 +8,7 @@ const locales: string[] = ["en-us", "ko", "ja", "zh-cn"];
 
 export function loadLanguages() {
     for (const lang in locales) {
-        const filePath = path.dirname(`../../localization/${lang.toLowerCase()}.json`);
+        const filePath = path.join(__dirname, `../../localization/${lang}.json`);
         console.log('reading file from ' + filePath);
         if (fs.existsSync(filePath)) {
           const fileContent = fs.readFileSync(filePath, 'utf8');
